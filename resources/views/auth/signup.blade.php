7dbd66c5469efaa1884337a6867f9561c18bcb20
ali <mdali2016.227@gmail.com>
2022-05-20 10:07:14 +0600

    [Add] header for auth layout

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -3,14 +3,6 @@
 @section('title', 'Sign Up - '.TITLE_BASE)
 
 @section('style')
-    <link rel="stylesheet" href="{{asset('assets/css/auth.css')}}">
-    <style>
-        .form-area {
-            height: 100px;
-            width: 100px;
-            border: 1px solid #222;
-        }
-    </style>
 @endsection
 
 @section('content')
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-md">
+            <a class="navbar-brand" href="#">Genie EMS</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{route('signup')}}">Sign Up</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
