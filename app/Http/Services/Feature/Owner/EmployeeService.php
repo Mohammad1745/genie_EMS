ee5117d13c1f5cbc55d32e62d9212c5af443f021
ali <mdali2016.227@gmail.com>
2022-05-20 16:24:06 +0600

    [Change] employee list method

--- app/Http/Services/Feature/Owner/EmployeeService.php
+++ app/Http/Services/Feature/Owner/EmployeeService.php
@@ -4,6 +4,7 @@ namespace App\Http\Services\Feature\Owner;
 
 use App\Http\Services\Base\UserService;
 use App\Http\Services\ResponseService;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class EmployeeService extends ResponseService
@@ -12,24 +13,42 @@ class EmployeeService extends ResponseService
      * @var UserService
      */
     private UserService $userService;
+    /**
+     * @var \App\Http\Services\Base\EmployeeService
+     */
+    private \App\Http\Services\Base\EmployeeService $employeeService;
 
     /**
      * AuthService constructor.
      * @param UserService $userService
+     * @param \App\Http\Services\Base\EmployeeService $employeeService
      */
-    public function __construct (UserService $userService)
+    public function __construct (UserService $userService, \App\Http\Services\Base\EmployeeService $employeeService)
     {
         $this->userService = $userService;
+        $this->employeeService = $employeeService;
     }
 
     /**
-     * @return mixed
+     * @return array
      */
-    public function employeeList (): mixed
+    public function employeeList (): array
     {
         try {
-            $employees = $this->userService->getWhere(['role' => EMPLOYEE_ROLE], ['id', 'username']);
-            return $employees->toArray();
+            $data = [];
+            $employees = Auth::user()->employees->all();
+
+            foreach ($employees as $employee) {
+                $user =$employee->user;
+
+                $data[] = [
+                    'id' => $employee->id,
+                    'user_id' => $user->id,
+                    'username' => $user->username
+                ];
+            }
+
+            return $data;
         } catch (\Exception $exception) {
             return [];
         }
@@ -44,10 +63,11 @@ class EmployeeService extends ResponseService
         try {
             DB::beginTransaction();
             $user = $this->userService->create( $this->userService->formatUserDataAsEmployee( $request->all()));
+            $employee = $this->employeeService->create( $this->employeeService->formatEmployeeData($user->id, Auth::id()));
 
             DB::commit();
 
-            return $this->response()->success(__("Successfully signed up as a ". userRoles( $user->role).". Verification Code has been sent to ".$user->email."."));
+            return $this->response()->success(__("Successfully added new employee."));
         } catch (\Exception $exception) {
             DB::rollBack();
 
