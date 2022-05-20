06f7748a0a6968d4178f6177a0559a3e98a3b823
ali <mdali2016.227@gmail.com>
2022-05-20 16:30:26 +0600

    [Add] employee middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Employee;
 use App\Http\Middleware\Owner;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -66,5 +67,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
         'owner' => Owner::class,
+        'employee' => Employee::class,
     ];
 }
